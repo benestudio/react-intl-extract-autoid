870c874237c766ee3469d663da24725d7fabf4d2
Gabor Paholics <gabor.paholics@benestudio.co>
2020-11-05 14:07:45 +0100

    step-1

---
+++ src/App/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import messages from './messages'
+import { FormattedMessage } from 'react-intl'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <FormattedMessage {...messages.hello} /> <FormattedMessage {...messages.world} />
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { IntlProvider } from 'react-intl';
+import enMessages from './messages/en.json'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <IntlProvider locale='en' defaultLocale='en' messages={enMessages}>
+      <App />
+    </IntlProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
